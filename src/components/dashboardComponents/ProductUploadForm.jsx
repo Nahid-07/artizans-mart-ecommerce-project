fbb031d042e3d3f1b37022c1e405ee11c19fa2be
Nahid-07 <nahidasif1998@gmail.com>
2025-08-26 18:04:19 +0600

    form data recvd

--- src/components/dashboardComponents/ProductUploadForm.jsx
+++ src/components/dashboardComponents/ProductUploadForm.jsx
@@ -1,19 +1,40 @@
 export const ProductUploadForm = () => {
+  const handleSubmit = e =>{
+    e.preventDefault();
+    const form = e.target;
+    const productData = {
+      "productName" : form.productName.value,
+      'category' : form.productCategory.value,
+      "price" : form.price.value,
+      "stock" : form.stock.value,
+      "ratings" : form.ratings.value,
+      "photourl" : form.photourl.value,
+      "description" : form.description.value,
+
+    }
+    console.log(productData)
+  }
   return (
     <div className="bg-white p-10 md:p-20 rounded-lg shadow-lg">
-      <h3 className="text-gray-800 text-xl font-medium text-center mb-3">Add a Product</h3>
-      <form>
+      <h3 className="text-gray-800 text-xl font-medium text-center mb-3">
+        Add a Product
+      </h3>
+      <form onSubmit={handleSubmit}>
         <div>
           <label className="block">Product Name</label>
           <input
             type="text"
             className="outline-none rounded-lg border border-sky-600 md:w-96 mt-2 p-2 text-gray-800"
             placeholder="Product name"
+            name="productName"
           />
         </div>
         <div>
           <label className="block mt-2">Category</label>
-          <select name="productCategory" className="outline-none rounded-lg border border-sky-600 md:w-96 mt-2 p-2 text-gray-800">
+          <select
+            name="productCategory"
+            className="outline-none rounded-lg border border-sky-600 md:w-96 mt-2 p-2 text-gray-800"
+          >
             <option value="powerbank">Powerbank</option>
             <option value="earbuds">earbuds</option>
             <option value="smartwatch">Smartwatch</option>
@@ -25,6 +46,7 @@ export const ProductUploadForm = () => {
             type="text"
             className="outline-none rounded-lg border border-sky-600 md:w-96 mt-2 p-2 text-gray-800"
             placeholder="Price"
+            name="price"
           />
         </div>
         <div>
@@ -33,6 +55,7 @@ export const ProductUploadForm = () => {
             type="number"
             className="outline-none rounded-lg border border-sky-600 md:w-96 mt-2 p-2 text-gray-800"
             placeholder="Price"
+            name="stock"
           />
         </div>
         <div>
@@ -43,6 +66,7 @@ export const ProductUploadForm = () => {
             placeholder="Price"
             step={0.1}
             max={5}
+            name="ratings"
           />
         </div>
         <div>
@@ -51,14 +75,20 @@ export const ProductUploadForm = () => {
             type="text"
             className="outline-none rounded-lg border border-sky-600 md:w-96 mt-2 p-2 text-gray-800"
             placeholder="Photo url"
+            name="photourl"
           />
         </div>
         <div>
           <label className="block mt-2">Description</label>
-          <textarea name="description" className="outline-none rounded-lg border border-sky-600 md:w-96 mt-2 p-2 text-gray-800"></textarea>
+          <textarea
+            name="description"
+            className="outline-none rounded-lg border border-sky-600 md:w-96 mt-2 p-2 text-gray-800"
+          ></textarea>
         </div>
         <div>
-          <button className="md:w-96 bg-sky-600 text-white p-3 rounded-lg mt-2">Add Product</button>
+          <button className="md:w-96 bg-sky-600 text-white p-3 rounded-lg mt-2">
+            Add Product
+          </button>
         </div>
       </form>
     </div>
